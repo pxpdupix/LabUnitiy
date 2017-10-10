18dd9ad436c5d0e365c477da63ffb15f7215d18b
pxpdupix <j.a.rueda@gmail.com>
2017-10-10 13:41:45 +0200

    10/10/2017 - 13:40

--- nuevo 3d/Assets/Scripts/MovementIfKey.cs
+++ nuevo 3d/Assets/Scripts/MovementIfKey.cs
@@ -4,22 +4,27 @@ using UnityEngine;
 
 public class MovementIfKey : MonoBehaviour
 {
+	[Range(-10.0f, 10.0f)]
     public float speed;
 
 	// Update is called once per frame
 	void Update ()
     {
 		if (Input.GetKey (KeyCode.W))
-			transform.Translate(Vector3.forward * speed * Time.deltaTime);
+			transform.Translate(0, 1.0f * speed * Time.deltaTime, 0);
 		if (Input.GetKey (KeyCode.S))
-			transform.Translate(Vector3.back * speed * Time.deltaTime);
+			transform.Translate(0, -1.0f * speed * Time.deltaTime, 0);
 		if (Input.GetKey (KeyCode.A))
 			transform.Translate(Vector3.left * speed * Time.deltaTime);
 		if (Input.GetKey (KeyCode.D))
 			transform.Translate(Vector3.right * speed * Time.deltaTime);
+//		if (Input.GetKey (KeyCode.Q))
+//			transform.Translate (-1.0f * speed * Time.deltaTime, 0, 1.0f * speed * Time.deltaTime);
+//		if (Input.GetKey (KeyCode.E))
+//			transform.Translate (1.0f * speed * Time.deltaTime, 0, 1.0f * speed * Time.deltaTime);
 		if (Input.GetKey (KeyCode.Q))
-			transform.Translate (-1.0f * speed * Time.deltaTime, 0, 1.0f * speed * Time.deltaTime);
+			transform.Rotate (0, 0, 5.0f * speed * Time.deltaTime);
 		if (Input.GetKey (KeyCode.E))
-			transform.Translate (1.0f * speed * Time.deltaTime, 0, 1.0f * speed * Time.deltaTime);
+			transform.Rotate (0, 0, -5.0f * speed * Time.deltaTime);
 	}
 }
