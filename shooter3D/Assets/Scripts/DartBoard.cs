0fadfa35bec4078eea6571aac7c2ea1a7207888b
pxpdupix <32553993+pxpdupix@users.noreply.github.com>
2017-10-19 20:51:49 +0200

    clase 19/10/2017 - 21:00

---
+++ shooter3D/Assets/Scripts/DartBoard.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DartBoard : MonoBehaviour
+{
+    public string killerTag;
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == killerTag)
+        {
+            Destroy(col.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ shooter3D/Assets/Scripts/Shooter3D.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooter3D : MonoBehaviour
+{
+    [Range(1, 10)]
+    public float shootSpeed = 1;
+    private float _shootTimer;
+
+    [Header("Input")]
+    public bool useLeftClick;
+    public bool useRightClick;
+    public KeyCode shootKey;
+    public string shootAxis;
+
+    [Header("Bullet")]
+    public GameObject bulletPrefab;
+    public Transform bulletStart;
+    public float lifeTime;
+
+    private AudioSource _audioSource;
+
+    void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    void Start()
+    {
+        _shootTimer = 0;
+    }
+	
+	void Update ()
+    {
+        _shootTimer -= Time.deltaTime;
+        if(_shootTimer <= 0)
+        {
+            if (Input.GetKey(shootKey) || (useLeftClick && Input.GetMouseButton(0)) || (useRightClick && Input.GetMouseButton(1)) || (shootAxis != "" && Input.GetAxis(shootAxis) > 0))
+            {
+                Fire();
+                _shootTimer = 1 / shootSpeed;
+            }
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject bullet = (GameObject)Instantiate(bulletPrefab, bulletStart.position, bulletStart.rotation);
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 15;
+        Destroy(bullet, lifeTime);
+        if (_audioSource != null)
+            _audioSource.Play();
+    }
+}
