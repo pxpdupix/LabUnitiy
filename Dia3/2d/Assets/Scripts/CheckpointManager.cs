bb71f39bdc3d1a14dfe6b501463ae4e5139e3eeb
dupix <jarueda@MacBook-Pro-de-Jose.local>
2017-11-02 23:43:24 +0100

    2/10/2017 - 23:43

---
+++ Dia3/2d/Assets/Scripts/CheckpointManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckpointManager : MonoBehaviour 
+{
+    public Vector3 checkPointPosition;
+
+    public void SetCheckPointPosition(Vector3 pos)
+    {
+        checkPointPosition = transform.position + pos;
+    }
+
+    public void LoadCheckpoint(GameObject obj)
+    {
+        obj.transform.position = checkPointPosition;
+        if (obj.GetComponent<Rigidbody2D>() != null)
+        {
+            obj.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+        }
+        if (obj.GetComponent<Health>() != null)
+        {
+            obj.GetComponent<Health>().RecoverAllHealth();
+        }
+    }
+}
---
+++ Dia3/Navmesh, cámaras e iluminación/Assets/Scripts/IA.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class IA : MonoBehaviour
+{
+    public Transform goal;
+    private NavMeshAgent _navMeshAgent;
+    private Animator _anim;
+
+    void Awake()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _anim = GetComponent<Animator>();
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        _navMeshAgent.SetDestination(goal.position);
+        _anim.SetFloat("Speed", _navMeshAgent.velocity.magnitude);
+    }
+}
